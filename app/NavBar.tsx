2169287861cc1c3722cf43f5f85d8bbb3f7d478e
idriss <idrissmouliom@gmail.com>
2024-02-14 10:38:03 +0100

    Adding Drop-down Menu

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Box, Container, Flex } from "@radix-ui/themes";
+import { Avatar, Box, Container, DropdownMenu, Flex, Text } from "@radix-ui/themes";
 import classnames from "classnames";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
@@ -38,7 +38,23 @@ const NavBar = () => {
           </ul>
         </Flex>
         <Box>
-        {status === "authenticated" && (<Link href='/api/auth/signout'>Logout</Link>)}
+        {status === "authenticated" && 
+          <DropdownMenu.Root>
+            <DropdownMenu.Trigger>
+              <Avatar size='2' src={session.user!.image!} fallback="?" className="cursor-pointer" radius="full"/>
+            </DropdownMenu.Trigger>
+            <DropdownMenu.Content>
+              <DropdownMenu.Label>
+                <Text size='2'>
+                  {session.user!.email!}
+                </Text >
+              </DropdownMenu.Label>
+              <DropdownMenu.Item>
+                <Link href='/api/auth/signin'>Logout</Link>
+              </DropdownMenu.Item>
+            </DropdownMenu.Content>
+          </DropdownMenu.Root>}
+
         {status === "unauthenticated" && (<Link href='/api/auth/signin'>Login</Link>)}
       </Box>
       </Flex>
