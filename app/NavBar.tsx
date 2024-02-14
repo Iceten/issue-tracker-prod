511a30e1d5f8203773b568c52265771c92fe9f74
idriss <idrissmouliom@gmail.com>
2024-02-14 10:19:35 +0100

    Change the Layout of the Navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Box } from "@radix-ui/themes";
+import { Box, Container, Flex } from "@radix-ui/themes";
 import classnames from "classnames";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
@@ -15,28 +15,36 @@ const NavBar = () => {
   const {status, data:session} = useSession()
   
   return (
-    <nav className="flex space-x-6 border-b mb-5 h-14 items-center px-5">
-      <Link href="/">
-        <AiFillBug />
-      </Link>
-      <ul className="flex space-x-6">
-        {links.map((link) => (
-          <li key={link.href}>
-          <Link
-            href={link.href}
-            className={classnames({
-              "text-zinc-900": currentPage === link.href,
-              "text-zinc-500": currentPage !== link.href,
-              "hover:text-zinc-800 transition-colors": true,
-            })}>
-            {link.label}
-          </Link></li>
-        ))}
-      </ul>
-      <Box>
+    <nav className="border-b mb-5 p-3 px-5">
+      <Container>
+      <Flex align='center' justify='between' gap='3'>
+        <Flex align='center' gap='3'>
+          <Link href="/">
+            <AiFillBug />
+          </Link>
+          <ul className="flex space-x-6">
+            {links.map((link) => (
+              <li key={link.href}>
+                <Link
+                  href={link.href}
+                  className={classnames({
+                  "text-zinc-900": currentPage === link.href,
+                  "text-zinc-500": currentPage !== link.href,
+                  "hover:text-zinc-800 transition-colors": true,
+                    })}>
+                  {link.label}
+                </Link></li>
+            ))}
+          </ul>
+        </Flex>
+        <Box>
         {status === "authenticated" && (<Link href='/api/auth/signout'>Logout</Link>)}
         {status === "unauthenticated" && (<Link href='/api/auth/signin'>Login</Link>)}
       </Box>
+      </Flex>
+      </Container>
+      
+    
     </nav>
   );
 };
